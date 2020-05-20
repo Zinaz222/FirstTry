deba764387e0a401a33d8dd7d121dc6b4166da64
Abdallah Hilali <abdallah.s.hilali@gmail.com>
2020-05-20 00:14:41 -0700

    This is code that is added to the profile and driver class

--- IdeaProjects/tryingToUnderstandTXTFIELD/src/FacebookClone/Driver.java
+++ IdeaProjects/tryingToUnderstandTXTFIELD/src/FacebookClone/Driver.java
@@ -1,4 +1,19 @@
 package FacebookClone;
 
-public class Driver {
-}
+
+public class Driver
+{
+    public static void main(String[] args)
+    {
+        ProfileManager m = new ProfileManager();
+
+        System.out.println("Creating profiles and the network.");
+
+        Profile john = new Profile();
+        john.setName("John", "Doe");
+        john.setStatus("Married");
+        m.addProfile(john);
+
+
+    } // end main
+} // end Drive
