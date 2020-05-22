697941843318e15d599e2e36d5d0581f944263d4
Hassaan Haq <haqh@uci.edu>
2020-05-21 17:31:11 -0700

    Made some changes

--- IdeaProjects/tryingToUnderstandTXTFIELD/src/FacebookClone/ProfileManager.java
+++ IdeaProjects/tryingToUnderstandTXTFIELD/src/FacebookClone/ProfileManager.java
@@ -3,49 +3,7 @@ package FacebookClone;
 import java.awt.image.*;
 import GraphPackage.*;
 import ADTPackage.*;
-/**
- An implementation of a profile manager on a simple social network.
 
- @author Jesse Grabowski
- @author Joseph Erickson
- @version 5.0 */
-public class ProfileManager {
-    private UndirectedGraph<Profile> allProfiles;
-
-    /**
-     * Constructor for an instance of a profile manager.
-     */
-    public ProfileManager() {
-        allProfiles = new UndirectedGraph<>();
-    } // end default constructor
-
-    /**
-     * Adds a profile to the social network.
-     *
-     * @param p The profile to be added to the network.
-     */
-    public void addProfile(Profile p) {
-        allProfiles.addVertex(p);
-    } // end addProfile
-
-    /**
-     * Creates a friendship between two users on the social network.
-     *
-     * @param a The first profile in the friendship.
-     * @param b The second profile in the friendship.
-     */
-    public void friendshipTransaction(Profile a, Profile b) {
-    } // end friendshipTransaction
-
-    /**
-     * Displays each profile's information and friends.
-     */
-    public void display(Profile startPoint) {
-    } // end ProfileManager
-}
-
-
-**************************************************************************************************************************
 package facenovelpackage;
 
 import java.util.ArrayList;
@@ -197,4 +155,6 @@ public class ProfileManager
             network.get(i).display();
         }
     }
-} // end ProfileManager
+}
+
+// end ProfileManager
